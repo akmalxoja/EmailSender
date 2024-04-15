6e0f9724a6e427d40af3d9e4d7ebe2ffbde389e1
Akmalxo'ja Sharifxo'jayev <akmalxojasharifxojayev@gmail.com>
2024-04-16 00:27:45 +0500

    moshniy

--- EmailSender.API/Program.cs
+++ EmailSender.API/Program.cs
@@ -1,14 +1,63 @@
 using EmailSender.Application;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(
+                   options =>
+                   {
+                       options.TokenValidationParameters = GetTokenValidationParameters(builder.Configuration);
+
+                       options.Events = new JwtBearerEvents
+                       {
+                           OnAuthenticationFailed = (context) =>
+                           {
+                               if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                               {
+                                   context.Response.Headers.Add("IsTokenExpired", "true");
+                               }
+                               return Task.CompletedTask;
+                           }
+                       };
+                   });
+
+
 builder.Services.AddApplication();
+
+
+static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+{
+    return new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        ValidAudience = configuration["JWT:ValidAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+        ClockSkew = TimeSpan.Zero,
+    };
+}
+
+
+
+
+
+
+
 
 
 var app = builder.Build();
